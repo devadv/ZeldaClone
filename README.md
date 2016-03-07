9b80c0c02a17c65bda4315bd9e3938dabfe6b53b
devadv <ansemsdevries@gmail.com>
2016-03-07 20:09:48 +0100

    README and TODO and added javadoc

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ZeldaClone
+
+Een gezamelijke game project:
+
+Nick is de hoofdontwikkelaar verantwoordelijk voor de gameplay en vormgeving en doet een groot gedeelte van het programeerwerk.
+Ben neemt het ontwerp/technische gedeelte voor zijn rekening en zal Nick zoveel mogelijk bij staan in het programeerwerk.
+Mogelijk zullen er gaandeweg de ontwikkeling van de game ook andere medewerkers betrokken worden die op verschillende gebieden een bijdrage kunnen leveren.  
--- src/gamedev/zeldaclone/states/ZeldaMenu.java
+++ src/gamedev/zeldaclone/states/ZeldaMenu.java
@@ -5,34 +5,31 @@ import org.newdawn.slick.state.*;
 
 public class ZeldaMenu extends BasicGameState {
 
-	public  ZeldaMenu(int i) {
-		
-	}
-	
-	@Override
-	public void init(GameContainer gc, StateBasedGame sbg)
-			throws SlickException {
-		// TODO Auto-generated method stub
+	public ZeldaMenu(int i) {
 
 	}
 
 	@Override
-	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
-			throws SlickException {
-		// TODO Auto-generated method stub
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		//TODO
 
 	}
 
 	@Override
-	public void update(GameContainer gc, StateBasedGame sbg, int delta)
-			throws SlickException {
-		// TODO Auto-generated method stub
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		//TODO
+
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+		//TODO
 
 	}
 
 	@Override
 	public int getID() {
-		// TODO Auto-generated method stub
+		// TODO 
 		return 0;
 	}
 
