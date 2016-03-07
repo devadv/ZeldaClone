6462e6f0f1262f83a3509499129854d543029144
devadv <ansemsdevries@gmail.com>
2016-03-07 20:09:48 +0100

    README and TODO and added javadoc

--- src/gamedev/zeldaclone/states/ZeldaGame.java
+++ src/gamedev/zeldaclone/states/ZeldaGame.java
@@ -8,30 +8,31 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class ZeldaGame extends BasicGameState {
 
-	@Override
-	public void init(GameContainer gc, StateBasedGame sbg)
-			throws SlickException {
-		// TODO Auto-generated method stub
+	public ZeldaGame(int i) {
 
 	}
 
 	@Override
-	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
-			throws SlickException {
-		// TODO Auto-generated method stub
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		// TODO 
 
 	}
 
 	@Override
-	public void update(GameContainer gc, StateBasedGame sbg, int delta)
-			throws SlickException {
-		// TODO Auto-generated method stub
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		// TODO 
+
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+		// TODO 
 
 	}
 
 	@Override
 	public int getID() {
-		// TODO Auto-generated method stub
+		// TODO 
 		return 0;
 	}
 
