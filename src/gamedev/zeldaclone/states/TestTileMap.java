af6133b37911d2bc3c673fb7a6b52003aa0440f4
devadv <ansemsdevries@gmail.com>
2016-03-13 12:05:46 +0100

    added method drawTiledIDs and variable layer/layerIndex

--- src/gamedev/zeldaclone/states/TestTileMap.java
+++ src/gamedev/zeldaclone/states/TestTileMap.java
@@ -15,26 +15,30 @@ import org.newdawn.slick.tiled.TiledMap;
 
 public class TestTileMap {
 
-	private TiledMap tilemap;
+	private TiledMap tileMap;
 	private Font awtFont;
 	private TrueTypeFont ttf;
-	private int TS = 32;
 	private boolean debug;
+	private int layerIndex;
+	private int TILESIZE = 32;
 
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-		tilemap = new TiledMap("res/testtilemap2.tmx");
+		tileMap = new TiledMap("res/testtilemap2.tmx");
 		awtFont = new Font("RetGanon", Font.PLAIN, 20);
 		ttf = new TrueTypeFont(awtFont, false);
+		String layer = "Wall";
+		layerIndex = tileMap.getLayerIndex(layer);
 	}
 
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		g.setColor(Color.red);
-		g.drawString("getLayerIndex Wall / Grass : " + tilemap.getLayerIndex("Wall") +" " + tilemap.getLayerIndex("Grass"), 492, 50);
-		g.drawString("getWidth: Width in tiles: " + tilemap.getWidth(),492, 70);
-		tilemap.render(0, 0);
+		g.drawString("getLayerIndex Wall / Grass : " + tileMap.getLayerIndex("Wall") +" " + tileMap.getLayerIndex("Grass"), 492, 50);
+		g.drawString("getWidth: Width in tiles: " + tileMap.getWidth(),492, 70);
+		tileMap.render(0, 0);
 		g.setBackground(Color.orange);
 		if(debug) {
-			drawDebugLines(g, TS);;
+			drawDebugLines(g, TILESIZE);
+			drawTiledIDs(g);
 		}
 
 	}
@@ -53,6 +57,15 @@ public class TestTileMap {
 			g.drawLine(0,  i,  resolutions, i);
 				
 			
+		}
+	}
+	public void drawTiledIDs(Graphics g) {
+		g.setColor(Color.black);
+		for (int i = 0; i < tileMap.getWidth(); i++) {
+			for (int j = 0; j < tileMap.getHeight(); j++) {
+				g.drawString("" + tileMap.getTileId(i, j, layerIndex), (i * TILESIZE ) + 5,
+						(j * TILESIZE) + 5);
+			}
 		}
 	}
 	
